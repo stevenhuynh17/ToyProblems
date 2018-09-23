ead10cfa70c9fca158e974070547c59c84b1b2b1
Steven Huynh <stevenhuynh17@gmail.com>
2018-09-22 21:19:19 -0700

    Update 10DaysOfJS.js
    day 2

--- 10DaysOfJS.js
+++ 10DaysOfJS.js
@@ -1,3 +1,5 @@
+// DAY 1
+
 function getArea(length, width) {
     let area;
     // Write your code here
@@ -27,3 +29,56 @@ function factorial(val) {
     }
     return value
 }
+
+function main() {
+    const PI = Math.PI
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let input = readLine() 
+    // Print the area of the circle:
+    console.log(PI * input * input)
+    // Print the perimeter of the circle:
+    console.log(2 * PI * input)
+    
+ // DAY 2
+    
+ function getGrade(score) {
+    let grade;
+    // Write your code here
+    if(25 < score && score <= 30) {
+        grade = 'A'
+    } else if(20 < score && score <= 25) {
+        grade = 'B'
+    } else if(15 < score && score <= 20) {
+        grade = 'C'
+    } else if(10 < score && score <= 15) {
+        grade = 'D'
+    } else if(5 < score && score <= 10) {
+        grade = 'E'
+    } else if(0 < score && score <= 5) {
+        grade = 'F'
+    }
+    return grade;
+}
+    
+function getLetter(s) {
+    let letter;
+    switch(true) {
+        case ("aeiou").includes(s[0]):
+            letter = 'A'
+            break
+        case ("bcdfg").includes(s[0]):
+            letter = 'B'
+            break
+        case ("hjklm").includes(s[0]):
+            letter = 'C'
+            break
+        case ("npqrstvwxyz").includes(s[0]):
+            letter = 'D'
+            break
+        default :
+            letter
+    }
+    
+    return letter;
+}
+    
