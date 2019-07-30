7c0add3201deadf6a70896ac6cccb35ff6977d10
Steven Huynh <stevenhuynh17@gmail.com>
2019-07-29 23:23:13 -0700

    Create sortArrayByParity.java
    started on problem, going reexamine the next day

---
+++ java/sortArrayByParity.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        ArrayList even = new ArrayList();
+        ArrayList odd = new ArrayList();
+        Object[] test = 
+        
+        for(int i=0; i<A.length; i++) {
+            System.out.println(A[i]);
+        }
+        return even;
+    }
+}
