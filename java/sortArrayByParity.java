ff03d0c0eec14bdc9ab9d4574be8100d75f3793b
Steven Huynh <stevenhuynh17@gmail.com>
2019-07-30 21:54:43 -0700

    Update sortArrayByParity.java
    4ms (10%) runtime, 41mb (86%) memory; working solution.

--- java/sortArrayByParity.java
+++ java/sortArrayByParity.java
@@ -1,12 +1,31 @@
 class Solution {
     public int[] sortArrayByParity(int[] A) {
+        // Initialize arraylist for dynamic use
         ArrayList even = new ArrayList();
         ArrayList odd = new ArrayList();
-        Object[] test = 
         
+        
+        // Differentiate between odd and even values
         for(int i=0; i<A.length; i++) {
-            System.out.println(A[i]);
+            int current = A[i];
+            if(current % 2 == 0) {
+                even.add(current);
+            } else {
+                odd.add(current);
+            }
         }
-        return even;
+        
+        // Add all odd values to the even 
+        for(int i=0; i<odd.size(); i++) {
+            even.add(odd.get(i));
+        }
+        
+        // Convert ArrayList into array to have proper return type
+        int[] answer = new int[even.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = (int) even.get(i);
+        }
+        
+        return answer;
     }
 }
