dcc3c56b9becaa9e507d961000839f92d2732110
Steven Huynh <stevenhuynh17@gmail.com>
2019-08-01 22:57:39 -0700

    Complete: 1ms(40%) runtime; 36.4mb(100%) memory

--- java/flipAndInvert.java
+++ java/flipAndInvert.java
@@ -2,17 +2,19 @@ class Solution {
     public int[][] flipAndInvertImage(int[][] A) {
         // Flip image
         for(int i=0; i<A.length; i++) {
-            Integer[] current = Integer.valueOf(A[i]);
-            System.out.println("BEFORE: " + Arrays.toString(current));
-            Collections.reverse(Arrays.asList(current));
-            System.out.println("AFTER: " + Arrays.toString(current));
-        }
-        System.out.println(Arrays.toString(A));
-        
-        
-        // Invert image
-        for(int i=0; i<A.length; i++) {
             int[] current = A[i];
+            int halfway = current.length/2;
+            int length = current.length - 1;
+            
+            for(int j=0; j<halfway; j++) {
+                int begin = current[j];
+                int end = current[length];
+                
+                current[j] = end;
+                current[length] = begin;
+                length--;   
+            }    
+            // Invert image
             for(int j=0; j<current.length; j++) {
                 if(current[j] == 1) {
                     current[j] = 0;
