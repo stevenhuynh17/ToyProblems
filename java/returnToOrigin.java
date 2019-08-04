aa41c58c746cd58eb1c3a8af7a01e9852a34dcb2
Steven Huynh <stevenhuynh17@gmail.com>
2019-08-04 00:26:59 -0700

    Complete: 783ms(5%) runtime; 37.9mb(100%) memory

--- java/returnToOrigin.java
+++ java/returnToOrigin.java
@@ -1,10 +1,35 @@
 class Solution {
     public boolean judgeCircle(String moves) {
+        // Initialize origin
+        int[] origin = new int[]{0,0};
+        
+        // Go through each direction
         for(int i=0; i<moves.length(); i++){
             char current = moves.charAt(i);
             System.out.println(current);
+            
+            // Add appropriate position value
+            switch(current){
+                case 'U':
+                    origin[0] += 1;
+                    break;
+                case 'D':
+                    origin[0] -= 1;
+                    break;
+                case 'R':
+                    origin[1] += 1;
+                    break;
+                case 'L':
+                    origin[1] -= 1;
+                    break;
+            }
         }
         
-        return true;
+        // Check if origin is at origin
+        if(origin[0] == 0 && origin[1] == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
