69e414f7283cc776b063bbebc035f77396d1bc3d
Steven Huynh <stevenhuynh17@gmail.com>
2019-08-03 23:47:33 -0700

    In progress

---
+++ java/returnToOrigin.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        for(int i=0; i<moves.length(); i++){
+            char current = moves.charAt(i);
+            System.out.println(current);
+        }
+        
+        return true;
+    }
+}
