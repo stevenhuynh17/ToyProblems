38a061e20e86f4d3bc668f8357dfeea6347ee92d
Steven Huynh <stevenhuynh17@gmail.com>
2019-08-05 17:05:53 -0700

    In Progress

---
+++ java/selfDividingNum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List result = new ArrayList();
+        
+        for(int i=left; i<right; i++) {
+           
+        }
+        
+        System.out.println(1 % 10);
+        
+        return result;
+    }
+}
