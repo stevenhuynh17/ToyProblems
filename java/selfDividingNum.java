39dbb707ef81b4401631962f5c5b0f666b837f0e
Steven Huynh <stevenhuynh17@gmail.com>
2019-08-28 11:42:31 -0700

    Complete

--- java/selfDividingNum.java
+++ java/selfDividingNum.java
@@ -1,11 +1,31 @@
 class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {
         LinkedList result = new LinkedList();
+        boolean status = true; 
         
         // Loop through from lower bound to higher bound
-        // For each digit of the int, see if it is divisible to the whole int
-        // If all are divisible, add to result linkedlist
+        for(int i=left; i<=right; i++) {
+            int current = i;
+            
+            // Extract the digits of the number
+            while(current > 0) {
+                int digit = current % 10; 
+                
+                // If the digit can't divide the number, break and move on
+                if(digit == 0 || i % digit != 0) {
+                    status = false;
+                    break;
+                }
+                current = current / 10;
+            }
+            if(status == false) {
+                status = true;
+                continue;
+            } 
+            result.add(i);    
+        }
         
+        // If all are divisible, add to result linkedlist        
         return result;
     }
 }
